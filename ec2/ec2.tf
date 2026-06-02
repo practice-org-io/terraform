80c3d3c90f0c26d2f571dda3de042d3d17f9e4af
lokesh <lokesh.theofficial@gmail.com>
2026-06-02 15:46:13 +0530

    some changes

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "example"{
     ami = ""
     instance_type = "t3.micro"
+    vpc_security_group_ids = [aws_security_group.allow_tls.id]
 
     tags = {
         Name = "terraform"
@@ -9,11 +10,26 @@ resource "aws_instance" "example"{
 }
 
 resource "aws_security_group" "allow_tls" {
-    name    = "allow_tls"
+    name    = "allow_all_terraform"
     description = "Allow TLS inbound traffic and all outbound traffic"
-    vpc_id  = aws_vpc.main.id
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
 
     tags = {
-        Name = "allow_tls"
+        Name = "allow-all-terraform"
     }
 }
