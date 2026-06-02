0f5eb6b6443c018a259ff7e2a679e1899591d529
lokesh <lokesh.theofficial@gmail.com>
2026-06-02 14:36:07 +0530

    some changes

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -1,9 +1,19 @@
 resource "aws_instance" "example"{
     ami = ""
-    instance_type = ""
+    instance_type = "t3.micro"
 
     tags = {
-        Name = ""
-        Project = ""
+        Name = "terraform"
+        Project = "roboshop"
+    }
+}
+
+resource "aws_security_group" "allow_tls" {
+    name    = "allow_tls"
+    description = "Allow TLS inbound traffic and all outbound traffic"
+    vpc_id  = aws_vpc.main.id
+
+    tags = {
+        Name = "allow_tls"
     }
 }
